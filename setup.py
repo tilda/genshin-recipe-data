9a219580038025d70551075c8a2ad7010d420e9f
tilda <iamtheworst@programmer.net>
2022-04-04 08:50:02 -0500

    add readme to setup (i thought it did that already?)

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup
 
+with open('README.md', 'r', encoding='utf-8') as file:
+    long_description = file.read()
+
 setup(
     name="genshinrecipes",
     version="2.6.0",
     author="tilda",
     author_email="tda@stairway.cf",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=["genshinrecipes"],
     python_requires=">=3.6",
     package_data={
